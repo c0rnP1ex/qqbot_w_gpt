1ad0c0f20b6af58bc5cad1b0045f99289c7d549c
c0rnP1ex <eholla@qq.com>
2023-03-21 17:56:56 +0800

    multi-session

--- plugins/chat.py
+++ plugins/chat.py
@@ -5,8 +5,8 @@ import json
 from datetime import datetime
 import gpt_api
 
-gpt_t = gpt_api.gpt_thread()
-
+public_session = gpt_api.gpt_thread()
+session_list = {}
 
 def readjson(filename):
     with open(filename, encoding='utf-8') as f:
@@ -26,24 +26,85 @@ def archive(data):
     write2json(filename, data)
 
 
-@on_command('ask', aliases=('ask', '/ask', '.', '。'))
-async def ask(session: CommandSession):
+@on_command('pask', aliases=('/pask', 'pask'))
+async def pask(session: CommandSession):
     prompt = session.current_arg_text.strip()
-    response = gpt_t.get_response(prompt)
+    response = public_session.get_response(prompt)
+    reply_msg = response['choices'][0]['message']['content']
+    await session.send(f'[public]\n{reply_msg}')
+
+
+@on_command('save', aliases=('/save', '.save', 'save'))
+async def save(session: CommandSession):
+    user_id = session.ctx['user_id']
+    if user_id not in session_list:
+        await session.send('你还没有和床爪说过话喵')
+    else:
+        archive(session_list[user_id].messages)
+        await session.send('已保存上一轮对话了喵。')
+
+
+@on_command('psave', aliases=('/psave', '.psave', 'psave'))
+async def psave(session: CommandSession):
+    archive(public_session.messages)
+    await session.send('[public]已保存上一轮对话了喵。')
+
+
+@on_command('reset', aliases=('/reset', '.reset', 'reset'))
+async def reset(session: CommandSession):
+    user_id = session.ctx['user_id']
+    if user_id not in session_list:
+        await session.send('你还没有和床爪说过话喵')
+    else:
+        archive(session_list[user_id].messages)
+        await session.send('已保存上一轮对话了喵')
+        session_list[user_id].reset_log()
+        await session.send('初始化了喵，现在床爪不记得任何事情。')
+
+
+@on_command('preset', aliases=('/preset', '.preset', 'preset'))
+async def preset(session: CommandSession):
+    archive(public_session.messages)
+    await session.send('[public]已保存上一轮对话了喵')
+    public_session.reset_log()
+    await session.send('[public]初始化了喵，现在床爪不记得任何事情。')
+
+
+@on_command('init', aliases=('/init', '.init', 'init'))
+async def init(session: CommandSession):
+    user_id = session.ctx['user_id']
+    if user_id not in session_list:
+        session_list[user_id] = gpt_api.gpt_thread()
+    else:
+        archive(session_list[user_id].messages)
+        await session.send('已保存上一轮对话了喵')
+    prompt = session.current_arg_text.strip()
+    session_list[user_id].reset_system_content(prompt)
+    await session.send(f'设定初始prompt为{prompt}')
+
+
+@on_command('pinit', aliases=('/pinit', '.pinit', 'pinit'))
+async def pinit(session: CommandSession):
+    archive(public_session.messages)
+    await session.send('[public]已保存上一轮对话了喵')
+    prompt = session.current_arg_text.strip()
+    public_session.reset_system_content(prompt)
+    await session.send(f'[public]设定初始prompt为{prompt}')
+
+
+
+@on_command('who', aliases=('/who', '.who'))
+async def who(session: CommandSession):
+    userID = session.ctx['user_id']
+    await session.send(str(userID))
+
+
+@on_natural_language
+async def default_(session: NLPSession):
+    user_id = session.ctx['user_id']
+    if user_id not in session_list:
+        session_list[user_id] = gpt_api.gpt_thread()
+    prompt = session.msg_text.strip()
+    response = session_list[user_id].get_response(prompt)
     reply_msg = response['choices'][0]['message']['content']
     await session.send(reply_msg)
-
-
-@on_command('save', aliases=('/save', 'save'))
-async def save(session: CommandSession):
-    archive(gpt_t.messages)
-    await session.send('已保存上一轮对话了喵。')
-
-
-@on_command('init', aliases=('/init', 'init'))
-async def init(session: CommandSession):
-    archive(gpt_t.messages)
-    await session.send('已保存上一轮对话了喵')
-    gpt_t.reset_log()
-    await session.send('初始化了喵，现在床爪不记得任何事情。')
-
